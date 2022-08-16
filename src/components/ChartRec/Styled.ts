b175d12b3c474554d30d4ee7193609d50a041ac2
sunchaehoon <sunchaehoon@naver.com>
2022-08-17 01:55:53 +0900

    ChartRec

---
+++ src/components/ChartRec/Styled.ts
@@ -0,0 +1,183 @@
+import styled from "styled-components";
+
+export const SectionHome = styled.div`
+    padding: 100px 80px 0 80px;
+    margin: 0 auto;
+    background-color: #fff;
+`;
+
+export const sectionInner =  styled.div`
+    padding-top: 10px;
+    max-width: 955px;
+    min-width: 600px;
+    margin: 0 auto;
+`;
+
+export const Recommendsection = styled.section`
+    display: block;
+    padding-top: 0!important;
+`;
+
+export const SectionContents = styled.div`
+    margin-bottom: 27px;
+    border-radius: 6px;
+    min-height: 189px;
+    margin-top: 6px;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    white-space: nowrap;
+`;
+
+export const ThumbnailWiper = styled.ul`
+    /* transition-duration: 0ms;
+    transform: translate3d(-1910px, 0px, 0px);
+    margin-top: -16px;
+    margin-left: 0;
+    overflow-x: visible;
+    overflow-y: visible; */
+
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-transition-property: -webkit-transform;
+    transition-property: -webkit-transform;
+    -o-transition-property: transform;
+    transition-property: transform;
+    transition-property: transform,-webkit-transform;
+    -webkit-box-sizing: content-box;
+    box-sizing: content-box;
+`;
+
+export const SwiperSlide = styled.li`
+    margin-top: 16px;
+    position: relative;
+    width: 955px;
+    overflow: visible;
+    margin: 0 auto;
+    font-size: 26.5px;
+
+    @media screen and (max-width: 900px) {
+        font-size: 25px;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 23.5px;
+    }
+`;
+
+export const RecommendHead = styled.div`
+    z-index: 4;
+    position: absolute;
+    width: 24%;
+    padding: 40px 0 0 40px;
+    font-size: 1em;
+    font-weight: 800;
+    line-height: 40px;
+    color: #fff;
+`;
+
+export const RecommendHeadH4 = styled.h4`
+    display: block;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-all;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    margin-bottom: 20px;
+
+    /* font-size: 26.8px; */
+    font-weight: 800;
+    line-height: 40px;
+    color: #fff;
+    user-select: none;
+`;
+
+export const TotalStatusData = styled.div`
+    color: #fff;
+    font-size: 14px;
+    font-weight: 100;
+    line-height: 20px;
+    user-select: none;
+`;
+
+export const BarNormal = styled.span`
+    padding: 0 10px;
+    font-size: 9px;
+    color: #fff;
+    opacity: .3;
+    user-select: none;
+`;
+
+export const AlbumList = styled.div`
+    z-index: 4;
+    position: absolute;
+    top: 30px;
+    left: 33.5%;
+`;
+
+export const RecommendTrackHome = styled.div`
+    position: relative;
+    height: 260px;
+    overflow: hidden;
+`;
+
+export const RecommendTrackList = styled.ul`
+    float: left;
+    li {
+        display: block;
+        height: 45px;
+        width: auto !important;
+        margin-top: 0;
+        margin-right: 0;
+        margin-left: 0;
+        padding: 10px 0;
+    }
+`;
+
+export const ThumbnailChart = styled.div`
+    display: inline-block;
+    font-size: 0px;
+    color: #fff;
+    vertical-align: top;
+`;
+
+export const ChartTitle = styled.strong`
+    margin: 5px 0 0 20px;
+    max-width: 210px;
+    width: 210px;  
+
+    display: inline-block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-weight: 400;
+    font-size: 16px;
+    font-family: 'GangwonEdu_OTFBoldA';
+
+    @font-face {
+        font-family: 'GangwonEdu_OTFBoldA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    @media screen and (max-width: 1050px) {
+        font-size: 12px;
+        width: 190px;
+    }
+`;
+
+export const ChartArtist = styled.span`
+    display: block;
+    margin-top: 4px;
+    font-size: 12px;
+    color: #fff;
+    opacity: .6;
+    @media screen and (max-width: 1050px) {
+        font-size: 9px;
+    }
+`;
--- src/components/MIL_Home/Home.tsx
+++ src/components/MIL_Home/Home.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
+import ChartRec from 'components/ChartRec/ChartRec';
 import * as S from './Styled';
 import { Link } from 'react-router-dom';
-import './style.css';
 import axios from 'axios';
 
 // export const getPosts = async () => {
@@ -11,100 +11,13 @@ import axios from 'axios';
 // };
 
 const Home = () => {
-   const [data, setData]: any = useState( [] );
-
-
-   const pgTitle = document.querySelector('title');
-   if (pgTitle instanceof Element) {
-      pgTitle.innerHTML = 'MIL | Home';
-   }
-
-   useEffect(() => {
-      axios.get('http://localhost:4000/BaladChart').then((response) => {
-         setData(response.data);
-      })
-      .catch(() => {console.log("something wrong.")});
-   }, []);
-
-
-
    return (
       <>
          <S.App>
             <Header />
 
             <S.Wrap>
-               <S.SectionHome>
-                  <S.sectionInner>
-                     <S.Recommendsection>
-                        <S.SectionContents>
-                           <S.ThumbnailWiper>
-                              <S.SwiperSlide>
-                                 <Link
-                                    className="MIL-recommend"
-                                    to="/detail/ballad"
-                                    title="오늘 퇴근하고 혼술하며 함께 할 인디 상세보기"
-                                 >
-                                    <S.RecommendHead>
-                                       <S.RecommendHeadH4>
-                                          오늘 퇴근하고 혼술하며 함께 할 인디
-                                       </S.RecommendHeadH4>
-                                       <S.TotalStatusData>
-                                          총 15곡
-                                          <S.BarNormal>|</S.BarNormal>
-                                          2022.6.20
-                                       </S.TotalStatusData>
-                                    </S.RecommendHead>
-                                    <S.AlbumList>
-                                       <S.RecommendTrackHome>
-                                          <S.RecommendTrackList>
-                                             
-                                             {
-                                                data.map(function(a: string | number, i: number): any {
-                                                   return (
-                                                      <li key={data[i].rank}>
-                                                         <S.ThumbnailChart>
-                                                            <img src={data[i].url} alt={`${data[i].artist} / ${data[i].url}`} className='chart_thumbnail' />
-                                                            <S.ChartTitle>
-                                                               {data[i].title}
-                                                               <S.ChartArtist>{data[i].artist}</S.ChartArtist>
-                                                            </S.ChartTitle>
-                                                         </S.ThumbnailChart>
-                                                      </li>
-                                                   );
-                                                })
-                                             }
-
-                                          </S.RecommendTrackList>
-
-                                          <S.RecommendTrackList className="recommend_track_list"> 
-                                             {
-                                                data.map(function(a: string | number, i: number): any {
-                                                   while(i >= 4) {
-                                                      return (
-                                                         <li key={data[i].rank}>
-                                                            <S.ThumbnailChart>
-                                                               <img src={data[i].url} alt={`${data[i].artist} / ${data[i].url}`} className='chart_thumbnail' />
-                                                               <S.ChartTitle>
-                                                                  {data[i].title}
-                                                                  <S.ChartArtist>{data[i].artist}</S.ChartArtist>
-                                                               </S.ChartTitle>
-                                                            </S.ThumbnailChart>
-                                                         </li>
-                                                      );
-                                                   }
-                                                })
-                                             }
-                                          </S.RecommendTrackList>
-                                       </S.RecommendTrackHome>
-                                    </S.AlbumList>
-                                 </Link>
-                              </S.SwiperSlide>
-                           </S.ThumbnailWiper>
-                        </S.SectionContents>
-                     </S.Recommendsection>
-                  </S.sectionInner>
-               </S.SectionHome>
+               <ChartRec />
             </S.Wrap>
          </S.App>
       </>
